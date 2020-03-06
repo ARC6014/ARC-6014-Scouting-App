6956f3028b94716811715c589c70318bcbc5f135
altalk23 <altalk.23@robcol.k12.tr>
2020-03-06 13:02:32 +0300

    initial

---
+++ android/app/src/main/kotlin/com/arc6014/scouting_app_rewrite/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.arc6014.scouting_app_rewrite
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
