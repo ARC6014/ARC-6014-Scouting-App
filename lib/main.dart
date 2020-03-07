5919a40c1dbd68fa28f88c6fe3a595555606fd46
altalk23 <altalk.23@robcol.k12.tr>
2020-03-07 13:56:26 +0300

    style start

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,32 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             title: 'Scouting App',
             theme: ThemeData(
-                primarySwatch: Colors.pink,
+                backgroundColor: Color(0xffe5def8),
+                primarySwatch: MaterialColor(
+                    0xfff451b0,
+                    <int, Color>{
+                        50: Color(0xfffce3f2),
+                        100: Color(0xfff7b8de),
+                        200: Color(0xfff488c8),
+                        300: Color(0xfff451b0),
+                        400: Color(0xfff5039b),
+                        500: Color(0xfff50084),
+                        600: Color(0xffe3007f),
+                        700: Color(0xffcb0079),
+                        800: Color(0xffb40073),
+                        900: Color(0xff8b006a),
+                    },
+                ),
+                cardTheme: CardTheme(
+                    margin: EdgeInsets.all(8),
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                            Radius.circular(6),
+                        ),
+                    ),
+                ),
+                fontFamily: 'Raleway',
             ),
             home: MyHomePage(title: 'Flutter Demo Home Page'),
         );
